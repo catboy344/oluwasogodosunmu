f688b0ba1791a9ca3ba7d5c76b259f788ab40e44
catboy344 <oluwasogodosunmu1@gmail.com>
2026-06-25 23:50:36 +0100

    Refactor main.jsx to use ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Site from "./Site.jsx";
-import Admin from "./Admin.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from './ThemeContext';
+import Site from './Site';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Site />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <Site />
+    </ThemeProvider>
   </React.StrictMode>
 );
