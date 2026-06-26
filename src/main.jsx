c2db3a65db76f6f43b850bd231c2e7222afb4dde
catboy344 <oluwasogodosunmu1@gmail.com>
2026-06-26 10:42:53 +0100

    Refactor main.jsx to use BrowserRouter and App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './ThemeContext';
-import Site from './Site';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <Site />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
